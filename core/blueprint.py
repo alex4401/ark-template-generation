87d4d794d0d2dd060bed1271047aee5f21c78941
alex4401 <rylatgl@gmail.com>
2020-09-28 19:28:58 +0200

    Add a class suffix if one is missing

--- core/blueprint.py
+++ core/blueprint.py
@@ -4,20 +4,25 @@ from .file import JsonData
 
 EXT_BP_CLASS = 'x-bp-class'
 
+
 def get_path(blueprint: JsonData) -> str:
     out = blueprint.get('bp', None)
     if not out:
         out = blueprint['blueprintPath']
     return out
 
+
 def get_class_name(blueprint: Union[JsonData, str]) -> str:
     if isinstance(blueprint, str):
         index = blueprint.index('.')
-        return blueprint[index + 1:]
+        out = blueprint[index + 1:]
+        if not out.endswith('_C'):
+            return out + '_C'
+        return out
     else:
         cached = blueprint.get(EXT_BP_CLASS, None)
         if cached:
             return cached
-           
+
         blueprint[EXT_BP_CLASS] = get_class_name(get_path(blueprint))
         return blueprint[EXT_BP_CLASS]
